c87acb27663bf203553e6a4a7c407b43fb7509dd
Oldman2563 <oldm2563@gmail.com>
2026-02-25 14:46:12 +0000

    make colocs more secure by adding tokens and add the invitation

--- app/Http/Controllers/AccommodationController.php
+++ app/Http/Controllers/AccommodationController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\InviteUserMail;
 use App\Models\Accommodation;
+use App\Models\Invitation;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class AccommodationController extends Controller
 {
@@ -13,17 +17,44 @@ class AccommodationController extends Controller
         $incomingFields = $request->validate([
             'name' => 'required'
         ]);
-        $user = auth()->user();
-        if(!$user->accommodations()->exists()){
-            $coloc = Accommodation::create($incomingFields);
-            $user->accommodations()->attach($coloc->id, ['role' => 'owner']);
-            return redirect('/coloc/' . $coloc->id);
+        $token = Str::random(12);
+
+        while (Accommodation::where('token', $token)->exists()) {
+            $token = Str::random(12);
         }
-        
+        $user = auth()->user();
+        if (!$user->accommodations()->exists()) {
+            $coloc = Accommodation::create([
+                'name' => $incomingFields['name'],
+                'token' => $token
+            ]);
+            $user->accommodations()->attach($coloc->id, ['role' => 'owner']);
+            return redirect('/coloc/' . $coloc->token);
+        }
     }
-    public function coloc(Accommodation $coloc){
+    public function coloc($token)
+    {
+        $coloc = Accommodation::where('token', $token)->first();
         $users = $coloc->users;
         return view('coloc', ['users' => $users, 'coloc' => $coloc, 'expenses' => []]);
     }
-    
+    public function invite(Request $request, Accommodation $coloc)
+    {
+        $incomingFields = $request->validate([
+            'email' => ['required', 'email']
+        ]);
+        $token = Str::random(50);
+
+        while (Invitation::where('token', $token)->exists()) {
+            $token = Str::random(50);
+        }
+        $invite = Invitation::create([
+            'accommodation_id' => $coloc->id,
+            'email' => $incomingFields['email'],
+            'token' => $token
+        ]);
+        Mail::to($incomingFields['email'])
+            ->send(new InviteUserMail(auth()->user()->name, $invite->token));
+        return back();
+    }
 }
