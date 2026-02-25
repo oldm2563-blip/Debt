25cbd18e4cbc81170af5e193b2efd747a6afac3b
Oldman2563 <oldm2563@gmail.com>
2026-02-25 10:25:05 +0000

    make the UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        if(auth()->user()->accommodations()->exists()){
+            $coloc = auth()->user()->accommodations()->first();
+            return redirect('/coloc/' . $coloc->id);
+        }
+        return view('newstart');
+    }
+}
