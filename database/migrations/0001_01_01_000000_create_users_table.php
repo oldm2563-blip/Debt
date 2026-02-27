3ad9b7b64bba5a15ad5364f89936b2be6f21f03e
Oldman2563 <oldm2563@gmail.com>
2026-02-27 10:31:09 +0000

    add some more columns

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_banned')->default(false);
+            $table->decimal('balance', 10, 2)->default(0);
+            $table->integer('reputation')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
