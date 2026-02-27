f28e395dda0922a35a50680294af6025ab51454c
Oldman2563 <oldm2563@gmail.com>
2026-02-27 14:22:03 +0000

    make the ban and the unban method's

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,8 +10,23 @@ class UserController extends Controller
     public function index(){
         if(auth()->user()->accommodations()->exists()){
             $coloc = auth()->user()->accommodations()->first();
+            if($coloc->state === 'canceled'){
+                return view('newstart');
+            }
             return redirect('/coloc/' . $coloc->token);
         }
+        auth()->user()->balance = 0.00;
+        auth()->user()->save();
         return view('newstart');
+    }
+    public function ban(User $user){
+        $user->is_banned = true;
+        $user->save();
+        return back();
+    }
+    public function unban(User $user){
+        $user->is_banned = false;
+        $user->save();
+        return back();
     }
 }
