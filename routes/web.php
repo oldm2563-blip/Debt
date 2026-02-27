2c4a24c8a8985752e889db3766a22201162df1bc
Oldman2563 <oldm2563@gmail.com>
2026-02-27 14:34:20 +0000

    add some more routes and gave a new middlewar to all of em

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccommodationController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\InviteController;
 use App\Http\Controllers\PaymentController;
@@ -12,22 +13,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified', 'banned'])->name('dashboard');
+Route::get('/ban/{user}', [UserController::class, 'ban'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/unban/{user}', [UserController::class, 'unban'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/create_coloc', [AccommodationController::class, 'create']);
-    Route::get('/coloc/{token}', [AccommodationController::class, 'coloc']);
-    Route::post('/invite/{coloc}', [AccommodationController::class, 'invite']);
-    Route::get('/request/{token}', [InviteController::class, 'form']);
-    Route::get('/accept/{token}', [InviteController::class, 'accept']);
-    Route::post('/categories/{coloc}', [AccommodationController::class, 'create_category']);
-    Route::post('/quit/{user}/{coloc}', [AccommodationController::class, 'quit']);
-    Route::post('/kick/{user}/{coloc}', [AccommodationController::class, 'kick']);
-    Route::post('/expenses/{coloc}', [ExpenseController::class, 'create_expense']);
-    Route::post('/pay/{id}/{exs_id}', [PaymentController::class, 'pay']);
+    Route::get('/profile', [ProfileController::class, 'edit'])->middleware(['banned'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->middleware(['banned'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->middleware(['banned'])->name('profile.destroy');
+    Route::post('/create_coloc', [AccommodationController::class, 'create'])->middleware(['banned']);
+    Route::get('/coloc/{token}', [AccommodationController::class, 'coloc'])->middleware(['banned']);
+    Route::post('/invite/{coloc}', [AccommodationController::class, 'invite'])->middleware(['banned']);
+    Route::get('/request/{token}', [InviteController::class, 'form'])->middleware(['banned']);
+    Route::get('/accept/{token}', [InviteController::class, 'accept'])->middleware(['banned']);
+    Route::post('/categories/{coloc}', [AccommodationController::class, 'create_category'])->middleware(['banned']);
+    Route::post('/quit/{user}/{coloc}', [AccommodationController::class, 'quit'])->middleware(['banned']);
+    Route::post('/kick/{user}/{coloc}', [AccommodationController::class, 'kick'])->middleware(['banned']);
+    Route::post('/cancel/{coloc}', [AccommodationController::class, 'cancel'])->middleware(['banned']);
+    Route::post('/expenses/{coloc}', [ExpenseController::class, 'create_expense'])->middleware(['banned']);
+    Route::post('/pay/{id}/{exs_id}', [PaymentController::class, 'pay'])->middleware(['banned']);
+    Route::get('/admin/dashboard', [AdminController::class, 'admin'])->middleware(['admin']);
 });
 
 require __DIR__.'/auth.php';
