33d36129a3244259cfeee7cbc16e18c5d6310e79
Oldman2563 <oldm2563@gmail.com>
2026-02-27 10:26:46 +0000

    add some more methods like kick, quit and add category and some conditions to prevent anyone for viewing withou access

--- app/Http/Controllers/AccommodationController.php
+++ app/Http/Controllers/AccommodationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Mail\InviteUserMail;
 use App\Models\Accommodation;
+use App\Models\Category;
+use App\Models\Expense;
 use App\Models\Invitation;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -34,9 +36,17 @@ class AccommodationController extends Controller
     }
     public function coloc($token)
     {
-        $coloc = Accommodation::where('token', $token)->first();
-        $users = $coloc->users;
-        return view('coloc', ['users' => $users, 'coloc' => $coloc, 'expenses' => []]);
+        if (auth()->user()->accommodations()->exists()) {
+            $coloc = Accommodation::where('token', $token)->first();
+            $users = $coloc->users;
+            $expenses = $coloc->expenses;
+            $payments = $coloc->payments;
+            $categories = $coloc->categories;
+            return view('coloc', ['users' => $users, 'coloc' => $coloc, 'expenses' => $expenses, 'payments' => $payments, 'categories' => $categories]);
+        }
+        else{
+            abort(403);
+        }
     }
     public function invite(Request $request, Accommodation $coloc)
     {
@@ -55,6 +65,75 @@ class AccommodationController extends Controller
         ]);
         Mail::to($incomingFields['email'])
             ->send(new InviteUserMail(auth()->user()->name, $invite->token));
+        return back();
+    }
+    public function create_category(Request $request, Accommodation $coloc)
+    {
+        $incomingFields = $request->validate([
+            'name' => 'required'
+        ]);
+        $incomingFields['accommodation_id'] = $coloc->id;
+        Category::updateOrCreate($incomingFields);
+        return back();
+    }
+
+
+
+    public function quit(User $user, Accommodation $coloc)
+    {
+        if ($user->balance >= 0) {
+            $user->reputation += 1;
+        } else {
+            $user->reputation -= 1;
+            $total = $user->sharrer()->sum('expense_share.amount');
+            $expense = Expense::create([
+                'title' => $user->name . 'left over',
+                'amount' => $total,
+                'category_id' => 2,
+                'paid_by' => auth()->id(),
+                'accommodation_id' => $coloc->id
+            ]);
+            $user->balance = 0;
+            $members = $coloc->users->where('id', '!=', $user->id);
+            $shared = $expense->amount / count($members);
+            foreach ($members as $member) {
+                $balance = $member->balance;
+                $balance = $balance - $shared;
+
+                $expense->shares()->attach($member->id, ['amount' => $shared]);
+                $member->balance = $balance;
+                $member->save();
+            }
+        }
+        $user->sharrer()->detach();
+        $user->accommodations()->detach($coloc->id);
+        $user->save();
+        return redirect('/dashboard');
+    }
+
+    public function kick(User $user, Accommodation $coloc)
+    {
+        if ($user->balance >= 0) {
+            $user->reputation += 1;
+        } else {
+            auth()->user()->reputation -= 1;
+            $total = $user->sharrer()->sum('expense_share.amount');
+            $expense = Expense::create([
+                'title' => $user->name . 'left over',
+                'amount' => $total,
+                'category_id' => 2,
+                'paid_by' => $user->id,
+                'accommodation_id' => $coloc->id
+            ]);
+            $user->balance = 0;
+            $expense->shares()->attach(auth()->id(), ['amount' => $total]);
+            auth()->user()->balance -= $total;
+            auth()->user()->save(); 
+        }
+
+        $user->sharrer()->detach();
+        $user->accommodations()->detach($coloc->id);
+        $user->save();
         return back();
     }
 }
